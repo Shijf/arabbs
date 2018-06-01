cfd221c1960a9d5c153d4f2979b91392a9fad99e
shijf <1035096900@qq.com>
2018-06-01 18:03:59 +0800

    add webhooks

---
+++ app/Http/Controllers/GithubWebhooksContriller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GithubWebhooksContriller extends Controller
+{
+    //
+
+    public function push()
+    {
+        return "It ok";
+    }
+}
