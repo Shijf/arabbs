b66d59db23160cf761ca24441d847ce02e9db50f
shijf <1035096900@qq.com>
2018-06-01 18:37:56 +0800

    test hook

--- app/Http/Controllers/GithubWebhooksContriller.php
+++ app/Http/Controllers/GithubWebhooksContriller.php
@@ -10,6 +10,14 @@ class GithubWebhooksContriller extends Controller
 
     public function push()
     {
-       return "OK";
+        $raw_post_data = file_get_contents('php://input', 'r');
+
+        echo "-------\$_POST------------------\n";
+
+        echo var_dump($_POST) . "\n";
+
+        echo "-------php://input-------------\n";
+
+        echo $raw_post_data . "\n";
     }
 }
