b03187ca14e821acd5c2e1c91350ec2a5b2720da
shijf <1035096900@qq.com>
2018-06-07 10:36:11 +0800

    add captcha

---
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Gregwar\Captcha\CaptchaBuilder;
+use App\Http\Requests\Api\CaptchaRequest;
+
+class CaptchasController extends Controller
+{
+    public function store(CaptchaRequest $request, CaptchaBuilder $captchaBuilder)
+    {
+        $key = 'captcha-'.str_random(15);
+        $phone = $request->phone;
+
+        $captcha = $captchaBuilder->build();
+        $expiredAt = now()->addMinutes(2);
+        \Cache::put($key, ['phone' => $phone, 'code' => $captcha->getPhrase()], $expiredAt);
+
+        $result = [
+            'captcha_key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(), //过期时间
+            'captcha_image_content' => $captcha->inline() //获取的 base64 图片验证码
+        ];
+
+        return $this->response->array($result)->setStatusCode(201);
+    }
+}
